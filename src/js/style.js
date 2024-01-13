ca64abdd5f548e624754d5322591bef262e57452
catalinbroinas <catalinbroinas@yahoo.com>
2024-01-13 17:24:36 +0200

    Add a delay when user click on link from navbar

--- src/js/style.js
+++ src/js/style.js
@@ -3,11 +3,21 @@ const scrollButton = document.querySelector('#scroll-button');
 window.addEventListener('scroll', () => {
     if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
         scrollButton.style.display = 'block';
-      } else {
+    } else {
         scrollButton.style.display = 'none';
-      }
+    }
 });
 scrollButton.addEventListener('click', () => {
     document.body.scrollTop = 0;    // For Safari
     document.documentElement.scrollTop = 0;     // For Chrome, Firefox, IE and Opera
 });
+
+function createTimedLink(element, callback, timeout) {
+    setTimeout(function () { callback(element); }, timeout);
+    return false;
+}
+
+function goToHref(element) {
+    /* Block of code, with no 'return false'. */
+    window.location = element.href;
+}
