045cb9e05c30eaaf392b97dc60efa62076c0e683
catalinbroinas <catalinbroinas@yahoo.com>
2024-01-13 19:13:13 +0200

    Add comments and refactor the code in style.js

--- src/js/style.js
+++ src/js/style.js
@@ -1,6 +1,7 @@
 const scrollButton = document.querySelector('#scroll-button');
 const navbarLinks = document.querySelectorAll('.navbar-link');
 
+// Display button after scrolling 350px
 window.addEventListener('scroll', () => {
     if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
         scrollButton.style.display = 'block';
@@ -8,13 +9,19 @@ window.addEventListener('scroll', () => {
         scrollButton.style.display = 'none';
     }
 });
+
+// Scroll to top
 scrollButton.addEventListener('click', () => {
     document.body.scrollTop = 0;    // For Safari
     document.documentElement.scrollTop = 0;     // For Chrome, Firefox, IE and Opera
 });
 
+/*
+Sets a timer to invoke a given callback function with a specific element 
+after a specified time interval
+*/
 function createTimedLink(element, callback, timeout) {
-    setTimeout(function () {
+    setTimeout(() => {
         callback(element);
     }, timeout);
     return false;
@@ -27,10 +34,9 @@ function goToHref(element) {
 
 // Add click event to each link
 function addClickEvent(link) {
-    link.addEventListener('click', function (event) {
+    link.addEventListener('click', (event) => {
         event.preventDefault(); // Avoid default redirection behavior
         createTimedLink(link, goToHref, 500);
     });
 }
-
 navbarLinks.forEach(addClickEvent);
